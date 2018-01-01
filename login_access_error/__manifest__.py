13b62af3b374598d416b22c378de932902bdb36f
gitmehedi <gitmehedi@gmail.com>
2018-01-01 15:42:47 +0600

    [ADD] Add new module for solving 'login_access' issue when 'website' installed.

---
+++ login_access_error/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Access Error Solution',
+    'category': 'Web',
+    'summary': 'Extend Website Application Error Solution',
+    'author': 'Md. Mehedi Hasan',
+    'website': 'https://github.com/gitmehedi',
+    'depends': ['website'],
+    'data': [],
+    'installable': True,
+    'auto_install': True,
+    'application': True,
+}
---
+++ login_access_error/main.py
@@ -0,0 +1,17 @@
+from odoo import http, _
+from odoo.http import request
+from odoo.addons.website.controllers.main import Website
+
+
+class ExtendWebsite(Website):
+
+    @http.route(website=True, auth="public")
+    def web_login(self, redirect=None, *args, **kw):
+        response = super(Website, self).web_login(redirect=redirect, *args, **kw)
+        if not redirect and 'login_success' in request.params:
+            if request.env['res.users'].browse(request.uid).has_group('base.group_user'):
+                redirect = '/web?' + request.httprequest.query_string
+            else:
+                redirect = '/'
+            return http.redirect_with_hash(redirect)
+        return response
