cc476da5b07a4d40d66bc7984a8786efb145a473
gitmehedi <git.mehedi@gmail.com>
2018-05-14 10:56:19 +0600

    [ADD] Add new report name opa_report which is a update version of previously named 'custom_report'. 'custom_report' module deleted from system.

---
+++ opa_report/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'OPA Custom Report',
+    'category': 'Reprot',
+    'summary': 'Modify existing header and footer',
+    'author': 'gitmehedi',
+    'website': 'https://angrybite.wordpress.com/',
+    'depends': ['report'],
+    'data': [
+        'data/paperformat.xml',
+        'views/templates.xml',
+        'views/inherite_layout.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
